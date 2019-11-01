73d287b9c438cc94f10f7c3e062390d6d2664165
ahmad firoz <ahmadf1788@gmail.com>
2019-11-01 10:54:31 +0530

    added android

---
+++ PlantDiseaseDetection/app/src/main/java/com/example/plantdiseasedetection/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.plantdiseasedetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.button);
+        imageview=(ImageView)findViewById(R.id.imageView);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent,0);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+        imageview.setImageBitmap(bitmap);
+    }
+}
